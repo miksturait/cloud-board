ff5afe30e667d45a0a7554bd3a4cab7fd2f26757
Swistak <swistaku@gmail.com>
2014-05-10 13:58:12 +0200

    Code cleanup

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,7 @@
 class Image < ActiveRecord::Base
   # --- Associations ---
   belongs_to :board
-<<<<<<< HEAD
   belongs_to :user
-=======
-  belongs_to :user, through: :board
   belongs_to :collection_image
   has_many :collections, through: :collection_image
->>>>>>> 22dd35ffa45c86de78f5c3622eb24ed819c5e7b7
 end
