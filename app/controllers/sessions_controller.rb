e00a1f4c9630ba604dd1ede6340523c381365f4b
lemoniak2 <l3moniak2@gmail.com>
2014-05-10 14:41:03 +0200

    Something added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create, :new, :destroy]
   # --- Actions ---
   def new
     redirect_to boards_path if current_user
